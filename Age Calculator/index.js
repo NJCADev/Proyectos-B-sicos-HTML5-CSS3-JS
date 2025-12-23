2dd66152586932aa325034aa4d8f8fc71f5ea35c
NJCADev <nelchacon1217@gmail.com>
2025-12-22 21:43:12 -0600

    Se termina el proyecto

--- Age Calculator/index.js
+++ Age Calculator/index.js
@@ -1,5 +1,6 @@
 let  userInput = document.getElementById("date");
 userInput.max = new Date().toISOString().split("T")[0];
+let result = document.getElementById("result");
 
 function calculateAge() {
     let birthDate = new Date(userInput.value);
@@ -18,7 +19,7 @@ function calculateAge() {
 
     y3 = y2 - y1;
 
-    if (m2 > m2) {
+    if (m2 >= m1) {
         m3 = m2 - m1;
     } else {
         y3 --;
@@ -36,6 +37,8 @@ function calculateAge() {
         m3 = 11;
         y3 --;
     }
+
+    result.innerHTML = `You are <span>${y3}</span> years, <span>${m3}</span> months and <span>${d3}</span> days old`;
 }
 
 function getDayInMonth(year, month){
