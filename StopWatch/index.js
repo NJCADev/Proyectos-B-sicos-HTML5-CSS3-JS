d94fa53bb6be6eb13c46c8144431ecfe29038e49
NJCADev <nelchacon1217@gmail.com>
2025-12-26 11:00:23 -0600

    Primeras actualizaciones

---
+++ StopWatch/index.js
@@ -0,0 +1,19 @@
+let [seconds, minutes, hours] = [0,0,0];
+let displayTime = document.getElementById("displayTime");
+
+function stopWatch() {
+    seconds ++;
+    if (seconds == 60) {
+        seconds = 0;
+        minutes ++;
+        
+        if (minutes == 60) {
+            minutes = 0;
+            hours ++;
+        }    
+    }
+}
+
+function watchStart() {
+    setInterval(stopWatch(), 1000);
+}
